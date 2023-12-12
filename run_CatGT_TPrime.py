0ce47a6d94fbcbc8bed1d714c88b3dc95d158839
Jake Ormond <j.ormond@ucl.ac.uk>
2023-12-12 17:42:56 +0000

    rewrote cristina's script for myself

--- run_CatGT_TPrime.py
+++ run_CatGT_TPrime.py
@@ -8,7 +8,7 @@ import numpy as np
 def main(basefolder, tprimesavefolder, runcatGT=True, runsupercat=True, runtprime=True):
     supercatcmd = catgt(basefolder, runcatGT=runcatGT)
     supercat(basefolder, supercatcmd, runsupercat=runsupercat)
-    tprime(basefolder, tprimesavefolder, runtprime=runtprime, runsupercat=runsupercat)
+    # tprime(basefolder, tprimesavefolder, runtprime=runtprime, runsupercat=runsupercat)
 
 
 def runprocess(command):
@@ -20,29 +20,33 @@ def runprocess(command):
 
 def catgt(basefolder, runcatGT=True):
     recordingfolders = glob.glob(basefolder + '/*_g*')
-    catgtcmd = 'C:/CatGT_newversion/CatGT'
+    print(recordingfolders)
+    # catgtcmd = 'C:/CatGT_newversion/CatGT'
+    catgtcmd = 'C:/Users/Jake/Documents/CatGT-win/CatGT.exe'
     supercatcmd = '-supercat='
+    catgt_output_dir = 'D:/CatGT_output'
 
     for f, ff in enumerate(recordingfolders):
         rundir = os.path.split(ff)[1]
         rundir = rundir[:-3]
         gvalue = ff[-1]
-        command = catgtcmd + ' -dir=' + basefolder + ' -run=' + rundir + ' -g=' + gvalue + ' -t=0 -ap -ni -prb_fld -prb=0'
-        command = command + ' -apfilter=butter,12,300,9000 -loccar_um=200,400 -gfix=0.4,0.1,0.02 -xid=0,0,0,1,0'
+        command = catgtcmd + ' -dir=' + basefolder + ' -run=' + rundir + ' -g=' + gvalue + ' -t=0 -ap -t_miss_ok -prb_fld -prb=0'
+        command = command + ' -apfilter=butter,12,300,9000 -gbldmx -gfix=0.4,0.1,0.02 -xd=2,0,-1,6,0 -dest=D:/CatGT_output'
         print(command)
 
-        if runcatGT:
-            runprocess(command)
+        #if runcatGT:
+        #      runprocess(command)
 
-        supercatpath = '{' + basefolder + ',' + os.path.split(ff)[1] + '}'
+        supercatpath = '{' + catgt_output_dir + ',' + os.path.split(ff)[1] + '}'
         supercatcmd = supercatcmd + supercatpath
 
     return supercatcmd
 
 
-def supercat(basefolder, supercatcmd, runsupercat=True):
-    catgtcmd = 'C:/CatGT_newversion/CatGT'
-    supercatcmd = catgtcmd + ' -t=cat -prb_fld -ap -prb=0 -no_auto_sync ' + supercatcmd + ' -dest=' + basefolder
+def supercat(supercatcmd, runsupercat=True):
+    catgtcmd = 'C:/Users/Jake/Documents/CatGT_win/CatGT'
+    supercat_dir = 'D:/supercat'
+    supercatcmd = catgtcmd + ' -t=cat -no_run_fld -ap -prb=0 -xd=2,0,-1,6,0 ' + supercatcmd + ' -dest=' + supercat_dir
 
     if runsupercat:
         runprocess(supercatcmd)
@@ -50,22 +54,7 @@ def supercat(basefolder, supercatcmd, runsupercat=True):
     print(supercatcmd)
 
 
-def tprime(basefolder, savefolder, runtprime=True, runsupercat=True):
-    tprimecmd = 'C:/TPrime/TPrime'
-    recordingfolders = glob.glob(basefolder + '/*_g*')
-
-    if runsupercat:
-        recordingfolders = recordingfolders[:-1]
-
-    for f, ff in enumerate(recordingfolders):
-        foldername = os.path.split(ff)[1]
-        foldername = foldername+'_imec0'
-        spikeglxsync = glob.glob(ff+'/'+foldername+'/*500.txt')[0]
-        nisync = glob.glob(ff+'/*500.txt')[0]
-        nicamsync = glob.glob(ff+'/*xid*txt')[0]
-        savefile = savefolder+foldername[:-6]+'_spikeglx.txt'
-        command = tprimecmd + ' -syncperiod=1.0 -tostream=' + spikeglxsync + ' -fromstream=1,' + nisync + ' -events=1,' + nicamsync + ',' + savefile
-        print(command)
-
-        if runtprime:
-            runprocess(command)
+if __name__ == "__main__":
+    raw_data_path = 'D:/rawData/Rat64-08-11-2023'
+    supercatcmd = catgt(raw_data_path, runcatGT=True)
+    supercat(supercatcmd, runsupercat=True)
